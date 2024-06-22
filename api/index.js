c8dfd5a97c6f51b667bd034e7aea649376b1e39f
hneelabh <hneelabh13@gmail.com>
2024-06-23 02:16:23 +0530

    Create New Post

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const User = require('./models/User')
+const User = require('./models/User');
+const Post = require('./models/Post');
 const bcrypt = require('bcryptjs');
 const app = express();
 const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
+const multer = require('multer');
+const uploadMiddleware = multer({dest: 'uploads/'});
+const fs = require('fs');
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfghjkiuytrew345rtgbju765rsdfgtree45'
@@ -67,6 +71,25 @@ mongoose.connect('mongodb+srv://hneelabh13:ld7ZH6j1Bu3NiAPn@cluster0.6o7ss0s.mon
 
     app.post('/logout', (req, res) => {
         res.cookie('token', '').json('ok');
+    })
+
+    app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
+        const {originalname, path}= req.file;
+        const parts = originalname.split('.');
+        const ext = parts[parts.length - 1];
+        const newPath = path+'.'+ext;
+        fs.renameSync(path, newPath);
+        res.json({files:req.file})
+
+        const {title, summary, content} = req.body;
+        const postDoc = await Post.create({
+            title,
+            summary,
+            content,
+            cover:newPath,
+        });
+        // res.json(postDoc);
+
     })
 
 
